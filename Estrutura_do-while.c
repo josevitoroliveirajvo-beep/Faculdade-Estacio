3d1ccdde348211d4d08064d6a4d811ed602d4df1
Jose Vitor <jose.vitor.oliveira.jvo@gmail.com>
2026-03-24 21:56:33 -0300

    Add do-while loop example in Estrutura_do-while.c
    Implement do-while loop to print numbers from 1 to 5.

---
+++ Estrutura_do-while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+
+  int i = 1;
+
+  do  {
+printf ("%d,\n", i);
+i++;    
+
+    
+  } while ( i <= 5); // ( i == 5) condição falsa mesmo assim fai printar o 1
+
+
+return 0;  
+}
