7a2253efe347f8c7b7610c08387d618afd284a8f
Jose Vitor <jose.vitor.oliveira.jvo@gmail.com>
2026-03-24 22:01:37 -0300

    Fix comment in do-while loop in Estrutura_do-while.c

--- Estrutura_do-while.c
+++ Estrutura_do-while.c
@@ -9,7 +9,7 @@ printf ("%d,\n", i);
 i++;    
 
     
-  } while ( i <= 5); // ( i == 5) condição falsa mesmo assim fai printar o 1
+  } while ( i <= 5); // ( i == 5) condição falsa mesmo assim vai printar o numero 1
 
 
 return 0;  
